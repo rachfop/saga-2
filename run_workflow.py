42c56d0272c64cb8758ef434be4f73fa273a608a
rachfop <prachford@icloud.com>
2024-06-07 10:20:06 -0700

    init

---
+++ run_workflow.py
@@ -0,0 +1,88 @@
+"""
+Module to run the workflow.
+"""
+
+import uuid
+
+from flask import Flask, jsonify, request
+from temporalio.client import Client
+
+from activities import BookVacationInput
+from saga_workflows import BookWorkflow
+from shared import TASK_QUEUE_NAME
+
+app = Flask(__name__)
+
+
+async def get_temporal_client() -> Client:
+    """
+    Connects to the Temporal server and returns a client instance.
+
+    Returns:
+        Client: Temporal client instance.
+    """
+    return await Client.connect("localhost:7233")
+
+
+@app.route("/book", methods=["POST"])
+async def book_vacation():
+    """
+    Endpoint to book a vacation.
+
+    Returns:
+        Response: JSON response with booking details or error message.
+    """
+    user_id = f'{request.json["name"].replace(" ", "-").lower()}-{uuid.uuid4().int % 1_000_000}'
+    attempts = request.json.get("attempts")
+    car = request.json.get("car")
+    hotel = request.json.get("hotel")
+    flight = request.json.get("flight")
+
+    input_data = BookVacationInput(
+        attempts=int(attempts),
+        book_user_id=user_id,
+        book_car_id=car,
+        book_hotel_id=hotel,
+        book_flight_id=flight,
+    )
+
+    client = await get_temporal_client()
+
+    result = await client.execute_workflow(
+        BookWorkflow.run,
+        input_data,
+        id=user_id,
+        task_queue=TASK_QUEUE_NAME,
+    )
+
+    response = {"user_id": user_id, "result": result}
+    if result == "Voyage cancelled":
+        response["cancelled"] = True
+    else:
+        try:
+            car_booking = (
+                result.split("Booked car: ")[1].split("Booked hotel: ")[0].strip()
+            )
+            hotel_booking = (
+                result.split("Booked hotel: ")[1].split("Booked flight: ")[0].strip()
+            )
+            flight_booking = result.split("Booked flight: ")[1].strip()
+
+            response.update(
+                {
+                    "cancelled": False,
+                    "car": car_booking,
+                    "hotel": hotel_booking,
+                    "flight": flight_booking,
+                }
+            )
+        except IndexError:
+            response[
+                "error"
+            ] = "Incomplete booking results. Please check the booking details."
+
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3002, debug=True)
---
+++ shared.py
@@ -0,0 +1,21 @@
+"""
+Module for shared data structures and constants.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class BookVacationInput:
+    """
+    Data class for storing input data for booking vacation.
+    """
+
+    book_user_id: str
+    book_car_id: str
+    book_hotel_id: str
+    book_flight_id: str
+    attempts: int
+
+
+TASK_QUEUE_NAME = "saga-task-queue"
